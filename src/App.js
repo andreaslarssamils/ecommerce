9ad3ec9e8d2657873a978e6b3ed67ab3086c6c80
Andreas Larssamils <larssamils.andreas@gmail.com>
2022-10-20 09:52:50 +0800

    update admin routes

--- src/App.js
+++ src/App.js
@@ -16,6 +16,14 @@ import UserCartDetailsPage from './pages/user/UserCartDetailsPage';
 
 // Protected Admin Pages
 import AdminUsersPage from './pages/admin/AdminUsersPage';
+import AdminEditUserPage from './pages/admin/AdminEditUserPage';
+import AdminProductsPage from './pages/admin/AdminProductsPage';
+import AdminCreateProductPage from './pages/admin/AdminCreateProductPage';
+import AdminEditProductPage from './pages/admin/AdminEditProductPage';
+import AdminOrdersPage from './pages/admin/AdminOrdersPage';
+import AdminOrderDetailsPage from './pages/admin/AdminOrderDetailsPage';
+import AdminChatsPage from './pages/admin/AdminChatsPage';
+import AdminAnalyticsPage from './pages/admin/AdminAnalyticsPage';
 
 function App() {
   return (
@@ -40,9 +48,20 @@ function App() {
       {/* ADMIN PROTECTED ROUTES */}
       <Route element={<ProtectedRoutesComponent admin={true} />}>
         <Route path="/admin/users" element={<AdminUsersPage />} />
-        <Route path="/user/my-orders" element={<UserOrdersPage />} />
-        <Route path="/user/cart-details" element={<UserCartDetailsPage />} />
-        <Route path="/user/order-details" element={<UserOrderDetailsPage />} />
+        <Route path="/admin/edit-user" element={<AdminEditUserPage />} />
+        <Route path="/admin/products" element={<AdminProductsPage />} />
+        <Route
+          path="/admin/create-new-product"
+          element={<AdminCreateProductPage />}
+        />
+        <Route path="/admin/edit-product" element={<AdminEditProductPage />} />
+        <Route path="/admin/orders" element={<AdminOrdersPage />} />
+        <Route
+          path="/admin/order-details"
+          element={<AdminOrderDetailsPage />}
+        />
+        <Route path="/admin/chats" element={<AdminChatsPage />} />
+        <Route path="/admin/analytics" element={<AdminAnalyticsPage />} />
       </Route>
     </Routes>
   );
